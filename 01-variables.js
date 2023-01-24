da1ff32687096736b8ab8d4e4b9035eae006bc89
PaolaSL12 <paola.sanchezl12@gamil.com>
2023-01-24 14:42:58 +0100

    add variables.js and intro of the theme

---
+++ 01-variables.js
@@ -0,0 +1,13 @@
+var greetings = "hola desde"
+var school = "pmba"
+var greetingsFromSchool = greetings + " " + school
+
+console.log(greetingsFromSchool)
+
+var ageAsString = '30'
+var ageAsNumber = 30
+var arrayOfValues = [30, '30', false, true, 4]
+var objectOfValues = {
+    age: 30
+    ageAsNumber: ageAsNumber
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# funsamentos JavaScript
+ 
+ Iniciacion a JavaScript. Fundamentos base:
+
+ - Variables
+ - Condiciones
+ - Bucles
+ - Scope y Hosting 
+ - Algoritmica basica
+ - Resolucion de problemas
+ 
